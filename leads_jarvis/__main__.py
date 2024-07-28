c41e903f5a72858d823f21ceabc932a34381e14b
ATATC <futerry@outlook.com>
2024-07-29 01:50:42 +0800

    Using hatchling.

--- leads_jarvis/__main__.py
+++ leads_jarvis/__main__.py
@@ -1,16 +1,5 @@
-from argparse import ArgumentParser as _ArgumentParser
-from sys import exit as _exit
+from leads_jarvis.__entry__ import __entry__
 
-from leads_jarvis.cli import main
-
-from leads import register_config as _register_config, load_config as _load_config
-from leads_gui import Config as _Config
 
 if __name__ == "__main__":
-    parser = _ArgumentParser(prog="LEADS Jarvis",
-                             description="Jarvis Extension for LEADS",
-                             epilog="GitHub: https://github.com/ProjectNeura/LEADS-Jarvis")
-    parser.add_argument("-c", "--config", default=None, help="specify a configuration file")
-    args = parser.parse_args()
-    _register_config(_load_config(args.config, _Config) if args.config else _Config({}))
-    _exit(main())
+    __entry__()
